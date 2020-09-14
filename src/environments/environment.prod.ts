908636ad8da7600edc41306abcb721a7b34dbc40
Juan Miramontes <juanjo_23@live.com>
2020-09-14 16:02:40 -0500

    Added firebase to environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC-g1IYCF_t9opUPZfN7x4YkllY4kbTJf0",
+    authDomain: "loteria-personajes.firebaseapp.com",
+    databaseURL: "https://loteria-personajes.firebaseio.com",
+    projectId: "loteria-personajes",
+    storageBucket: "loteria-personajes.appspot.com",
+    messagingSenderId: "592551790194",
+    appId: "1:592551790194:web:4cbe96a1bc2988e836a4eb"
+  }
 };
